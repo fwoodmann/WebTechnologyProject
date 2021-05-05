5ae09bc94f651ee94af10d9c05348ae8c8e319cc
da-albrecht <albrecht.david3@gmail.com>
2021-05-05 15:24:40 +0200

    added models/user.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ const user = require("./models/user");
 const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb://localhost:27017/socialMedia_db",
-  {useNewUrlParser: true}
+  {useNewUrlParser: true},
+  {useUnifiedTopology: true}
   );
 const db = mongoose.connection;
 
--- models/user.js
+++ models/user.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 const loginSchema = mongoose.Schema({
     email: String,
     password: String
-  })
+  });
   
   module.exports = mongoose.model("User", loginSchema);
