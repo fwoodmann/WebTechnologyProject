d794dc1d67c7a3d3da9d8bcf8ccce41fb01b51fe
da-albrecht <albrecht.david3@gmail.com>
2021-05-23 19:27:03 +0200

    added validators and findUser function

--- models/user.js
+++ models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    id: String,
-    username: String,
-    email: String,
-    password: String
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password:{  //password restrictions?
+        type: String,
+        required: true
+    }
 });
+
+UserSchema.methods.findUser = function () {
+    return this.model("User").find({username: this.username}).exec();
+}
 
 module.exports = mongoose.model("User", UserSchema);
