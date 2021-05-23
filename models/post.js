16e7d5887b812191c1ed0b1214c3fd83ff622db8
da-albrecht <albrecht.david3@gmail.com>
2021-05-23 19:51:39 +0200

    added validators and findPost instance method

--- models/post.js
+++ models/post.js
@@ -1,9 +1,25 @@
 const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema({
-    author: String,
-    context: String,
-    comments: String,
-    rating: Number
+    author:{
+        type: String,
+        required: true
+    }, 
+    context:{
+        type: String,
+        required: true
+    }, 
+    comments:{
+        type: String,
+
+    }, 
+    rating:{
+        type: Number
+
+    } 
 });
+
+postSchema.methods.findPost = function () {
+    return this.model("Post").find({author: this.author}).exec();
+}
 module.exports = mongoose.model("post", postSchema);
