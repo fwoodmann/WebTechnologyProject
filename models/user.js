192830b2bb4b40d0468a119b4f28aa423c6d9b37
da-albrecht <albrecht.david3@gmail.com>
2021-06-05 16:20:05 +0200

    added hashing and passwordComparison

--- models/user.js
+++ models/user.js
@@ -23,6 +23,24 @@ UserSchema.methods.findUser = function () {
     return this.model("User").find({
         username: this.username
     }).exec();
-}
+},
+
+UserSchema.pre("save", function(next) {
+    let user = this;
+
+    bcrypt.hast(user.password, 10).then(hash => {
+        user.password = hash;
+        next();
+    })
+    .catch(error => {
+        console.log(`Error in hashing password: ${error.message}`);
+        next(error);
+    })
+});
+
+UserSchema.methods.passwordComparison = function(inputPassword){
+    let user = this;
+    return bcrypt.compare(inputPassword, user.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
